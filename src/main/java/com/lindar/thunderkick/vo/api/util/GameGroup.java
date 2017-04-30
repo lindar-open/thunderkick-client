c18028be45b9763ce54779888f77a37829e01bec
Iulian Dafinoiu <cicorongho@yahoo.com>
2017-04-30 12:56:49 +0100

    add game group ref

--- src/main/java/com/lindar/thunderkick/vo/api/util/GameGroup.java
+++ src/main/java/com/lindar/thunderkick/vo/api/util/GameGroup.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 @Data
 public class GameGroup {
+    private String ref; // only used internally to identify new game groups
     private List<String> games; // a list of game ids provided by thunderkick
     private List<String> distributionChannels; // can be WEB or MOBILE but for now only WEB is supported
     private List<BetConfig> betConfigurations;
