804b5a378e2c13b4b4e2bc1f8ee51fe88849c4ed
David <david@lindar.com>
2018-09-17 16:57:38 +0100

    Added used by mobile verification

--- src/main/java/com/lindar/thunderkick/vo/api/FreeRoundsTemplateBase.java
+++ src/main/java/com/lindar/thunderkick/vo/api/FreeRoundsTemplateBase.java
@@ -10,4 +10,5 @@ public class FreeRoundsTemplateBase extends ErrorResponse {
     private List<GameGroup> gameGroups;
     private Boolean         usedForReferAFriend;
     private Boolean         usedForWelcomeOffer;
+    private Boolean usedForMobileVerification;
 }
