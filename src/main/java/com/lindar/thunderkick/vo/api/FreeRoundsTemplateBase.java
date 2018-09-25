b1432d5c752a83936f2d7b2bd9931e528db40fcd
Dafinoiu Iulian <cicorongho@yahoo.com>
2018-09-25 17:47:20 +0100

    update dependencies

--- src/main/java/com/lindar/thunderkick/vo/api/FreeRoundsTemplateBase.java
+++ src/main/java/com/lindar/thunderkick/vo/api/FreeRoundsTemplateBase.java
@@ -10,5 +10,5 @@ public class FreeRoundsTemplateBase extends ErrorResponse {
     private List<GameGroup> gameGroups;
     private Boolean         usedForReferAFriend;
     private Boolean         usedForWelcomeOffer;
-    private Boolean usedForMobileVerification;
+    private Boolean         usedForMobileVerification;
 }
