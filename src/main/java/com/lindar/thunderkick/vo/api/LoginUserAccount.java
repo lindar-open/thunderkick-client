6bebfdfaa8f4de0bc6dbdbed0b6e43852cc2feb2
Stefano <stefano@mrq.com>
2024-07-04 16:34:16 +0200

    add max bet to LoginUserAccount

--- src/main/java/com/lindar/thunderkick/vo/api/LoginUserAccount.java
+++ src/main/java/com/lindar/thunderkick/vo/api/LoginUserAccount.java
@@ -8,5 +8,5 @@ public class LoginUserAccount {
     private String userName;
     private String password; // this password is different than user's password
     private String operatorSessionToken; // unique id for user's session. It will be provided to Thunderkick and they will return it on each wallet call
-    private MaxBet maxBet; // Optional. Max bet player can place when placing a single bet. Can be used to for regulatory purposes in UK.
+    private MaxBet maxBet; // Optional. Max bet player can place when placing a single bet. Can be use to for regulatory purposes in UK.
 }
